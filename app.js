843c193511d781d18c9f6def553ffd6be5b0441d
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-14 14:00:56 -0600

    add phrase functionallity

--- app.js
+++ app.js
@@ -30,12 +30,15 @@ middleSelect.addEventListener('change', () => {
 });
 
 pantsSelect.addEventListener('change', () => {
+    // > set the character pants from the value of the pants <select>
     character.pants = pantsSelect.value;
+    // > redisplay the character
     displayCharacter();
 });
 
 function displayCharacter() {
     headImage.src = 'assets/character/' + character.head + '-head.png';
+    // > set the middle and pants images .src property based on the character middle and pants properties
     middleImage.src = 'assets/character/' + character.middle + '-middle.png';
     pantsImage.src = 'assets/character/' + character.pants + '-pants.png';
 }
@@ -46,15 +49,21 @@ function displayPhrases() {
 
     for (const phrase of character.phrases) {
         // > create an <li> element
+        const li = document.createElement('li');
         // > set the text of the li to the phrase
+        li.textContent = phrase;
         // > append the <li> to the phrases list
+        phraseList.append(li);
     }
 }
 
 addButton.addEventListener('click', () => {
     // > Get the phrase from the value of the phrase input
+    const phrase = phraseInput.value;
     // > Use ".push" to add the phrase to the end of the character phrases array
+    character.phrases.push(phrase);
     // > Re-display the phrases
+    displayPhrases();
 
     // reset the input
     phraseInput.value = '';
